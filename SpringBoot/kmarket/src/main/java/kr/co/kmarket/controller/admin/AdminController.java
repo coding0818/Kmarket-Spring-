e457b6d096986b18d97ac354398e89dd24820891
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-02-08 19:11:37 +0900

    hwang

---
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminController.java
@@ -0,0 +1,12 @@
+package kr.co.kmarket.controller.admin;
+
+import kr.co.kmarket.service.IndexService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class AdminController {
+
+    private IndexService service;
+}
---
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/dao/IndexDAO.java
@@ -0,0 +1,14 @@
+package kr.co.kmarket.dao;
+
+import kr.co.kmarket.vo.CateVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface IndexDAO {
+
+    public List<CateVO> selectCate(int cate1);
+}
