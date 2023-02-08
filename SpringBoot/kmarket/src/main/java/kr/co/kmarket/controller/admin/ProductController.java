d6ba65861f01064237c5f0e94dd3bf5e98eda63e
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-02-08 19:41:49 +0900

    2023/02/08 Index

--- SpringBoot/kmarket/src/main/java/kr/co/kmarket/controller/admin/ProductController.java
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/controller/admin/ProductController.java
@@ -1,13 +1,17 @@
 package kr.co.kmarket.controller.admin;
 
+import kr.co.kmarket.service.IndexService;
 import kr.co.kmarket.service.admin.ProductService;
+import kr.co.kmarket.vo.CateVO;
 import kr.co.kmarket.vo.ProductVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -15,8 +19,14 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @Autowired
+    private IndexService inservice;
+
     @GetMapping("admin/product/register")
     public String register(){
+        //List<CateVO> cate =inservice.selectCate(cate1);
+        //model.addAttribute("cate", cate);
+
         return "admin/product/register";
     }
 
