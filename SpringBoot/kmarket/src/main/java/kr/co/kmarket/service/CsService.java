ed3783835aad58ddd4163f0ec96cec3e85840e89
jojuyeong03 <jojuyeong03@gmail.com>
2023-02-09 17:48:19 +0900

    2023/02/09 메인화면 게시물처리 구현 중

---
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/service/CsService.java
@@ -0,0 +1,15 @@
+package kr.co.kmarket.service;
+
+import kr.co.kmarket.dao.CsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsService {
+
+    @Autowired
+    private CsDAO dao;
+
+
+
+}
