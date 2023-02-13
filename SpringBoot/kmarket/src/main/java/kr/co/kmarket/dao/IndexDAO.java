96bddf5c8d1ae230ee1cdc5e66f5061da4bbf775
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-02-13 14:09:42 +0900

    2023/02/13 mainIndex

--- SpringBoot/kmarket/src/main/java/kr/co/kmarket/dao/IndexDAO.java
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/dao/IndexDAO.java
@@ -2,6 +2,7 @@ package kr.co.kmarket.dao;
 
 import kr.co.kmarket.vo.CateVO;
 
+import kr.co.kmarket.vo.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,16 @@ import java.util.List;
 @Repository
 public interface IndexDAO {
 
+    // 관리자 2차 카테고리 설정
     public List<CateVO> selectCate(int cate1);
+
+    // 관리자 1차 카테고리 설정
     public List<CateVO> selectCate1();
     public List<CateVO> selectCategory();
+
+    // index aside 카테고리 분류
+    public List<CateVO> selectCates();
+
+    // index 상품분류(productDAO로 이동 시켜야함)
+    public List<ProductVO> selectIndex();
 }
