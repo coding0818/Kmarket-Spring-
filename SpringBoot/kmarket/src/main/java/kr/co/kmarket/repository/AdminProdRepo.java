ecbc0fe2af4d97668a3e580fe8efa5925123c96d
Yiwonjeong <near_and_dear_studio@naver.com>
2023-02-15 19:30:41 +0900

    2023/02/15 19:30

--- SpringBoot/kmarket/src/main/java/kr/co/kmarket/repository/AdminProdRepo.java
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/repository/AdminProdRepo.java
@@ -2,7 +2,6 @@ package kr.co.kmarket.repository;
 
 import kr.co.kmarket.entity.ProdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,12 +13,9 @@ public interface AdminProdRepo extends JpaRepository<ProdEntity, Integer> {
     // <엔티티, PK로 지정한 컬럼의 데이터 타입>
 
     // findBy(컬럼 이름)Containing : 컬럼에서 키워드가 포함된 것을 찾기
-    // 상품명 키워드 검색 + 페이징
-    //List<ProdEntity> findProdEntityBySeller(String seller);
+    List<ProdEntity> findBySeller(String seller);
 
-    List<ProdEntity> findBySellerAndProdNameContains(String seller, String prodName);
-
-    @Query("select pe from ProdEntity as pe")
-    public List<ProdEntity> selectProducts();
+    //@Query("select pe from ProdEntity as pe")
+    //public List<ProdEntity> selectProducts();
 
 }
