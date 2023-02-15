18cebee94d1bb4ae9bf6ec67c9c4aeea97a128b5
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-02-15 13:50:20 +0900

    2023/02/15 view수정

--- SpringBoot/kmarket/src/main/java/kr/co/kmarket/dao/ProductDAO.java
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/dao/ProductDAO.java
@@ -13,11 +13,17 @@ import java.util.List;
 public interface ProductDAO {
 
     // cate별 상품리스트 조회하기
-    public List<ProductVO> selectProducts(@Param("cate1") String cate1, @Param("cate2") String cate2);
+    public List<ProductVO> selectProducts(@Param("cate1") String cate1, @Param("cate2") String cate2, @Param("start") int start);
 
     // 상품번호로 상품 조회하기
     public ProductVO selectProduct(String prodNo);
 
     // 상품 네비게이션
     public CateVO selectNavCate(@Param("cate1") String cate1, @Param("cate2") String cate2);
+
+    // 페이징처리
+    public int selectCountProduct(@Param("cate1") String cate1, @Param("cate2") String cate2);
+
+
+
 }
