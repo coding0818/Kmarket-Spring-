20c6440fcf82cae3ec92a6e362c70ed7793394a0
jojuyeong03 <jojuyeong03@gmail.com>
2023-02-21 17:16:03 +0900

    2023/02/21 admin cs 페이징 처리, write 완료

--- SpringBoot/kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminNoticeController.java
+++ SpringBoot/kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminNoticeController.java
@@ -25,19 +25,21 @@ public class AdminNoticeController {
         List<CsVO> articles = null;
         long total = 0;
 
+        /*** 페이징 처리 ***/
+        int currentPage = service.getCurrentPage(pg);
+        int start = service.getLimitstart(currentPage);
+
         /*** selectbox로 검색한 것이 아닌 경우 ***/
-        if (cate2 == null){
-            articles = service.selectCSArticlesAll(cate1, 0);
-            total = service.getTotalCount(cate1, "all", null);
+        if ("all".equals(cate2)){
+        articles = service.selectCSArticlesAll(cate1, start);
+        total = service.getTotalCount(cate1, "all", null);
         /*** 검색어가 있을 경우 ***/
         } else {
-            articles = service.selectCsArticles(cate1, cate2, 0);
+            articles = service.selectCsArticles(cate1, cate2, start);
             total = service.getTotalCount(cate1, cate2, null);
         }
 
         /*** 페이징 처리 ***/
-        int currentPage = service.getCurrentPage(pg);
-        int start = service.getLimitstart(currentPage);
         int lastPage = service.getLastPageNum(total);
         int pageStartNum = service.getPageStartNum(total, start);
         int groups[] = service.getPageGroup(currentPage, lastPage);
@@ -49,14 +51,16 @@ public class AdminNoticeController {
         model.addAttribute("groups", groups);
         model.addAttribute("cate1", cate1);
         model.addAttribute("cate2", cate2);
+        model.addAttribute("pg", pg);
         return "admin/cs/notice/list";
     }
 
     @GetMapping("admin/cs/notice/modify")
-    public String modify(Model model, int csNo, String pg) {
+    public String modify(Model model, String cate2, int csNo, String pg) {
         CsVO art = service.selectCsArticle(csNo);
 
         model.addAttribute("art", art);
+        model.addAttribute("cate2", cate2);
         model.addAttribute("pg", pg);
         return "admin/cs/notice/modify";
     }
@@ -65,7 +69,7 @@ public class AdminNoticeController {
     public String modify(CsVO vo) {
         service.updateCsArticle(vo);
         int csNo = vo.getCsNo();
-        return "redirect:/admin/cs/notice/view?csNo="+csNo;
+        return "redirect:/admin/cs/notice/view?pg=1&cate2=all&csNo="+csNo;
     }
 
     @GetMapping("admin/cs/notice/view")
@@ -86,7 +90,7 @@ public class AdminNoticeController {
     public String write(CsVO vo) {
         vo.setCate1("notice");
         service.insertCsArticle(vo);
-        return "redirect:/admin/cs/notice/list";
+        return "redirect:/admin/cs/notice/list?cate2=all&pg=1";
     }
 
 }
